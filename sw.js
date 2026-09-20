9181c93f5bf8c7624821a222ff5e118f11382ee7
99HannibalLecter <119416336+99HannibalLecter@users.noreply.github.com>
2026-09-20 17:50:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'liubai-v6';
+const CACHE = 'liubai-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
